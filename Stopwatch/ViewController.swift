6c87289a0f58450bfe65e7fc61f0c40b558a1238
Aiden Lee <aidenlee@Aidens-MacBook-Pro.local>
2019-08-29 07:09:00 -0400

    First commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Aiden Lee on 8/29/19.
+//  Copyright © 2019 Aiden Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
